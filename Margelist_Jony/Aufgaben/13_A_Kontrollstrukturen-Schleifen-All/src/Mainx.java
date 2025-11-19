5cefb94a2b25b609ca52151edd179df4eeab4d94
Breevon <safal_chittilappilly@proton.me>
2025-11-19 11:06:22 +0100

    Done some work on Aufgabe 13 (Not fully finished) and added new one (Aufgabe 14)

--- Margelist_Jony/Aufgaben/13_A_Kontrollstrukturen-Schleifen-All/src/Mainx.java
+++ Margelist_Jony/Aufgaben/13_A_Kontrollstrukturen-Schleifen-All/src/Mainx.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Mainx {
     public static void main(String[] args) {
 
         //--------------------------------------------------------------------------------------------------------------
@@ -16,12 +16,48 @@ public class Main {
         //
         //      Hint2: Check if your implementation is correct. For example; the Year 1900 is not a leap year.
 
+        int leapYear100;
+        int leapYear400;
+        for (int todayYearCounter=0; todayYearCounter <= 2025; todayYearCounter+=4) {
+
+            leapYear100 = todayYearCounter%100;
+            leapYear400 = todayYearCounter%400;
+
+            if (leapYear100==0) {
+
+                    if (leapYear400==0){
+                        System.out.println(todayYearCounter);
+                    } else {
+
+                    }
+
+            } else {
+                System.out.println(todayYearCounter);
+            }
+        }
+
         //--------------------------------------------------------------------------------------------------------------
         System.out.println("Exercise 02");
         // 02.  Print out all numbers from 1-10.
         //      If a number is divisible by three, then print "Fizz".
         //      If a number is divisible by five, then print "Buzz".
         //      If a number is divisible by three and five, print "FizzBuzz".
+
+        for (int numberCounter=1 ; numberCounter <= 10; numberCounter+=1) {
+
+            int divisible3 = numberCounter%3;
+            int divisible5 = numberCounter%5;
+
+            if (divisible3==0 && divisible5==0){
+                System.out.println("FizzBuzz");
+            } else if (divisible3==0) {
+                System.out.println("Fizz");
+            } else if (divisible5==0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(numberCounter);
+            }
+        }
 
 
         //--------------------------------------------------------------------------------------------------------------
@@ -30,12 +66,25 @@ public class Main {
         //      Example: n = 7 -> "It's a prime number!"
         //      Example: n = 10 -> "It's not a prime number!"
 
-        int n = 7; // change this value from prime to not prime to test
+        int n = 121; // change this value from prime to not prime to test
         // Your code here
-
-
+         boolean isPrime = true;
+         for (int counter = 2; counter < n; counter++) {
+             if (n % counter == 0) {
+                 isPrime=false;
+                 break;
+             }
+         }
+         if(isPrime) {
+             System.out.println(n + " ist eine prime");
+         } else {
+             System.out.println(n + " ist keine prime");
+         }
         //--------------------------------------------------------------------------------------------------------------
         System.out.println("Exercise 04");  // Challenge+!
         // 04. Print all prime numbers from 2-100.
+
+
+
     }
 }
