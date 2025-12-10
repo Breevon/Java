1daa2e44840051df4ee3b2d0d48654a61fa4ceb3
Breevon <safal_chittilappilly@proton.me>
2025-12-10 08:34:20 +0100

    Aufgabe 16 (done)

---
+++ Margelist_Jony/Aufgaben/16_A_IndividualleKlasse/src/MinecraftPlayer.java
@@ -0,0 +1,19 @@
+public class MinecraftPlayer {
+
+    String favouriteMob = "Pandas";
+    String stupidMob = "Bats";
+    String strongestMob = "Ender Dragon";
+
+    public MinecraftPlayer(){
+
+    }
+
+    public void printFavouriteMob() {
+        System.out.println(favouriteMob);
+    }
+
+    public void printStupidMob() {
+        System.out.println(stupidMob);
+    }
+
+}
